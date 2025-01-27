0ca428306d12373ef31718457a2a970839fdb3c2
Tanvi Muley <tanvimuley27@gmail.com>
2025-01-27 10:10:49 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() 
+{
+    char ch;
+    printf("Enter the alphabet: ");
+    scanf("%c",&ch);
+   if ( ch=='a' || ch=='e' || ch=='i' || ch=='o' || c=='u') 
+        printf("%c is a vowel.\n", ch);
+   else 
+        printf("%c is not a vowel.\n", ch);
+}
+/* Enter the alphabet: u
+u is a vowel. */
